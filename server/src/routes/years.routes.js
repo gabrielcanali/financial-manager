efe418b0f0f188551481ac14b330f03b3d5aa473
Gabriel Canali <gabrielcanali.dev@gmail.com>
2026-01-18 13:13:04 -0300

    AI is hallucinating, redo everything from scratch (again)

--- server/src/routes/years.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import summaryController from "../controllers/summary.controller.js";
-
-const router = Router();
-
-router.get("/:year/summary", summaryController.getYearSummary);
-
-export default router;
